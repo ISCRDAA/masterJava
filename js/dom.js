696a2fe4703f399e041f37cc49d38931816ac67c
Angel Alonso Rodriguez <55955398+ISCRDAA@users.noreply.github.com>
2024-06-04 18:20:06 -0600

    aprendisaje para manipular el dom
    Aprendimos a saber como hacer cosas de html desde java script

---
+++ js/dom.js
@@ -0,0 +1,42 @@
+'use strict'
+//Vamos a comenzar a manipular el dom
+// El siguiente fragmento es para conseguir elementos por su id
+function cambiaColor(color){
+
+    caja.style.background=color;
+}
+//var caja =document.getElementById("micaja");
+var caja= document.querySelector("#micaja");
+caja.innerHTML="Texto desde java script";
+caja.style.background="red";
+caja.style.padding="20px";
+caja.className="Hola mexico";
+
+console.log(caja);
+
+//conseguir los elementos por su etiqueta
+var divs=document.getElementsByTagName("div");
+var seccion=document.querySelector("#misection");
+var hr =document.createElement("hr");
+
+
+console.log(divs);
+//divs.array.forEach((valor, indice) => {
+    var valor;
+    for(valor in divs){
+    console.log(divs[valor]);
+        if(typeof divs[valor].textContent =="string"){
+
+            var parrafo=document.createElement("p")
+            var texto=document.createTextNode(divs[valor].textContent);
+            parrafo.append(texto);
+            seccion.appendChild(parrafo);
+        }
+    
+    }
+    seccion.append(hr);
+
+//});
+
+
+
