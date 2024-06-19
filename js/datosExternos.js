2b61fb6442c1d1de4a4784ee95bff96dc7003c7b
Angel Alonso Rodriguez <55955398+ISCRDAA@users.noreply.github.com>
2024-06-18 18:47:53 -0600

    estamos en las promesas
    error y una promesa dentro de otra promesa

--- js/datosExternos.js
+++ js/datosExternos.js
@@ -4,25 +4,61 @@
 var usuarios =[];
 var div_usuarios=document.querySelector("#usuarios")
 var div_janet=document.querySelector("#janet")
+var div_profesor= document.querySelector("#profesor");
 
 getUsuarios()
 .then(data => data.json())
 .then(users => {
 lista_usuarios(users);
 
+   
+    return getInfo();
+})
+.then(data =>{
+    div_profesor.innerHTML=data;
     return getJanet();
-
 })
 .then(data => data.json())
 .then(janet => {
-mostrar_janet(janet.data);
+    mostrar_janet(janet.data);
 })
+.catch(err=>{
+    alert("error en las peticiones: "+err.message);
+})
+
+    
+
+
 
 
 
 function getUsuarios(){
 
     return fetch('https://jsonplaceholder.typicode.com/users');
+}
+
+function getInfo(){
+    var profesor ={
+        nombre:"Manuel",
+        apellido:"Torres",
+        edad:29,
+        pelicula:"Batman"
+    };
+
+    return new Promise((resolve, reject) => {
+        var profesor_string="";
+       
+
+        setTimeout(function(){
+            profesor_string=JSON.stringify(profesor);
+            if(typeof profesor_string !="string" || profesor_string=="")return reject("Error");
+                return resolve(profesor_string);
+                
+
+        },3000);
+
+        
+    });
 }
 
 function getJanet(){
@@ -33,7 +69,7 @@ function getJanet(){
 function lista_usuarios(usuarios){
     usuarios.map((user,i)=>{
         let nombre= document.createElement('h2');
-        nombre.innerHTML = i + user.name+" "+user.username;
+        nombre.innerHTML = i+ ".- " + user.name+" "+user.username;
         div_usuarios.appendChild(nombre);
 
         document.querySelector(".loading").style.display="none";
@@ -43,8 +79,13 @@ function lista_usuarios(usuarios){
 function mostrar_janet(janet){
     
         let nombre= document.createElement('h2');
+        let avatar= document.createElement('img');
         nombre.innerHTML = janet.first_name+""+janet.last_name;
+        avatar.src= janet.avatar;
+        avatar.width="100";
+        
         div_janet.appendChild(nombre);
+        div_janet.appendChild(avatar);
         document.querySelector("#janet .loading").style.display="none";
 
 
